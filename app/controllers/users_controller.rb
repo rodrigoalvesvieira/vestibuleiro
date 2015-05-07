027bc7c5b5c54185d9b48329f066fa45cb2a791f
Samuel Paz Mendes <pazspm@gmail.com>
2015-05-07 00:55:07 -0300

    Changed indentation to spaces instead of tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,30 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
+    def index
+        @users = User.all
+    end
 
-	def edit
-		@user = User.find(params[:id])
-	end
-	
-	def update
-		@user = User.find(params[:id])
-    	respond_to do |format|
-      		if @user.update(user_params)
-        		format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        		format.json { head :no_content }
-      		else
-       	 		format.html { render action: 'edit' }
-        		format.json { render json: @user.errors, status: :unprocessable_entity }
-     		end
-    	end
- 	end
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        respond_to do |format|
+            if @user.update(user_params)
+                format.html { redirect_to @user, notice: 'User was successfully updated.' }
+                format.json { head :no_content }
+            else
+                format.html { render action: 'edit' }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+        end
+    end
 
 
-	private 
-		def user_params
-			params.require(:user).permit(:name,:email,:school_year,:desired_course,:city,:avatar)
-		end
+    private
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def user_params
+            params.require(:user).permit(:name,:email,:school_year,:desired_course,:city,:avatar)
+        end
 
 end
