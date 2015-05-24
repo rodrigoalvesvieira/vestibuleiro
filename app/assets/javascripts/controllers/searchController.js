591d7629977c00aa0a762dcd39c1ca5e3ab523d2
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-05-23 22:26:04 -0300

    Saving up work

---
+++ app/assets/javascripts/controllers/searchController.js
@@ -0,0 +1,8 @@
+app.controllers.controller('SearchController', ['$scope', '$location', '$resource', '$http', 'QuestionService', function($scope, $location, $resource, $http, TripsService) {
+
+  $scope.searchInfo = {};
+
+  $scope.performSearch = function(searchInfo) {
+  };
+
+}]);
