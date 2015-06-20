04837f6c48a63b64315fb2f4ea5bd81501e7d2b6
Rodrigo Alves <rodrigo.alves@toptal.com>
2015-06-20 10:59:30 -0300

    Adding welcoming div to the home page for when the user is not logged in.

--- app/assets/javascripts/vestibuleiro.js
+++ app/assets/javascripts/vestibuleiro.js
@@ -1,40 +1,6 @@
 'use strict';
 
 var app = angular.module("Vestibuleiro", [
-  'ngSanitize',
-  'ui.router',
-  'ngResource',
-  'ngAnimate',
-  'pascalprecht.translate',
-  'ngCookies',
-  'LocalStorageModule',
-  'Vestibuleiro.controllers',
-  'Vestibuleiro.services',
-  'angular-loading-bar',
-  'ui.date',
-  'textAngular'
-]);
-
-app.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  '$locationProvider',
-  '$translateProvider',
-  '$httpProvider',
-  'localStorageServiceProvider',
-
-  function($stateProvider, $urlRouterProvider, $locationProvider, $translateProvider, $httpProvider, localStorageServiceProvider) {
-
-    // Enable CORS
-    $httpProvider.defaults.useXDomain = true;
-
-    // Local Storage config
-    localStorageServiceProvider.setPrefix('Vestibuleiro');
-
-    // I18n config
-    $translateProvider.useUrlLoader('locales/pt_BR.json');
-    $translateProvider.preferredLanguage('pt_BR');
-  }
 ]);
 
 app.controllers = angular.module("Vestibuleiro.controllers", []);
