72735b43e0656273dcf1f29cd38ba6062154aff8
rafaelfccg <mras@cin.ufpe.br>
2015-07-18 16:09:55 -0300

    cookie storing tab, to help paginate

--- public/bower/controllers/questionsController.js
+++ public/bower/controllers/questionsController.js
@@ -1,11 +1,31 @@
 app.controller('QuestionsController', ['$scope', function($scope) {
-  $scope.currentTab = 1;
+ 
+  $scope.saveTab = function(){
+  	localStorage.setItem('currentTab', $scope.currentTab);
+  }
+
+  $scope.getTab = function(){
+  	$scope.currentTab = localStorage.getItem('currentTab');
+  	if($scope.currentTab == null) {
+  		$scope.currentTab = 1;
+  		localStorage.setItem('currentTab', $scope.currentTab);
+  	}
+  }
+  $scope.currentTab = $scope.getTab();
 
   $scope.selectTab = function(setTab){
+
     $scope.currentTab = setTab;
+    $scope.saveTab();
   };
 
   $scope.isSelected = function(checkTab){
-    return $scope.currentTab === checkTab;
+  	$scope.getTab();
+  	alert('is selected ' + checkTab + ' scope: ' + $scope.currentTab);
+    return $scope.currentTab == checkTab;
   }
+
+
+
+
 }]);
