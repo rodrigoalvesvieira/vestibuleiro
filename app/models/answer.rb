049d595a31998cb53b3ebdbc80a5b3d9f735156d
Rodolfo Jose <rodolfoj.soares@gmail.com>
2015-07-18 15:15:43 -0300

    add controller for answers and add ajax for the upvote and downvote answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -30,6 +30,19 @@ class Answer
     return @result
   end
 
+  def add_voter(user)
+    self.analytics.users_id_vote += [user.id]
+    self.analytics.save
+  end
+
+  def is_voter(user)
+    self.analytics.users_id_vote.each do |user_id_vote|
+      return true if user_id_vote == user.id
+    end
+
+    return false
+  end
+
   class << self
     ## Takes a string and returns all answers from the database
     ## whose title or body contain the term
